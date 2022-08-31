5449f462653cc6565ed2c1df935e993aa2d26294
mlubbersdev <31659937+mlubbersdev@users.noreply.github.com>
2022-08-31 14:44:59 +0200

    Add files via upload

--- Maintenance_WinUpdate_DiskCleanup_CitrixOptimizer_FSecureScan.ps1
+++ Maintenance_WinUpdate_DiskCleanup_CitrixOptimizer_FSecureScan.ps1
@@ -17,8 +17,8 @@ $diskcleanup = {
 Write-Host 'Clearing CleanMgr.exe automation settings.'
 Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\*' -Name StateFlags0001 -ErrorAction SilentlyContinue | Remove-ItemProperty -Name StateFlags0001 -ErrorAction SilentlyContinue
 
-Write-Host 'Enabling Update Cleanup. This is done automatically in Windows 10 via a scheduled task.'
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Update Cleanup' -Name StateFlags0001 -Value 2 -PropertyType DWord
+#Write-Host 'Enabling Update Cleanup. This is done automatically in Windows 10 via a scheduled task.'
+#New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Update Cleanup' -Name StateFlags0001 -Value 2 -PropertyType DWord
 
 Write-Host 'Enabling Temporary Files Cleanup.'
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Temporary Files' -Name StateFlags0001 -Value 2 -PropertyType DWord
